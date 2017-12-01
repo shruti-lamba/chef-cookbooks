5135fb132699498fa0d7590f86cf55ec542730ac
shruti <shruti.lamba@tothenew.com>
2017-12-01 12:24:59 +0530

    hostname cookbook changes

--- cookbooks/hostname/recipes/default.rb
+++ cookbooks/hostname/recipes/default.rb
@@ -3,9 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-include_recipe 'recipe[ec2-tags-ohai-plugin]'
+#include_recipe 'recipe[ec2-tags-ohai-plugin]'
 
-hname = node['ec2']['tags']['Name']
+#hname = node['ec2']['tags']['Name']
 
 execute 'test' do
   command "echo #{node['ec2']['tags']['Name']} >> /home/ubuntu/test"
