5819f197698657c66120bf846594afc6a8ea107c
shruti <shruti.lamba@tothenew.com>
2017-12-06 15:15:17 +0530

    add encrypt cookbook

--- cookbooks/encrypt/recipes/default.rb
+++ cookbooks/encrypt/recipes/default.rb
@@ -2,21 +2,16 @@
 enc_key = ''
 key_name = 'secret_key'
 log 'Copying enc databag key at node'
-enc_key = data_bag_item('chef_credentials', 'encryption_key')['key']
-file "/etc/chef/#{key_name}" do
-  content enc_key
-  sensitive true
-  owner 'root'
-  group 'root'
-  mode '0600'
-  action :create
+execute 'download key' do
+  command 'https://s3.ap-south-1.amazonaws.com/girnarsoft-installs/secret_file /etc/chef/secret_key'
+  action :run
+  not_if { File.exist?("/etc/chef/secret_key") }
   notifies :run, 'bash[add entry to client.rb]'
-  not_if { File.exist?("/etc/chef/#{key_name}") }
 end
 bash 'add entry to client.rb' do
   user 'root'
   code <<-EOH
-  echo 'encrypted_data_bag_secret "/etc/chef/#{key_name}"'
+  echo 'encrypted_data_bag_secret "/etc/chef/secret_key"'
   EOH
   action :nothing
 end
