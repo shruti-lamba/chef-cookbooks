3da275d1b9d6035a4cb506fd6078b9308a8ffd1d
shruti <shruti.lamba@tothenew.com>
2017-12-06 15:18:05 +0530

    add encrypt cookbook

--- cookbooks/encrypt/recipes/default.rb
+++ cookbooks/encrypt/recipes/default.rb
@@ -3,7 +3,7 @@ enc_key = ''
 key_name = 'secret_key'
 log 'Copying enc databag key at node'
 execute 'download key' do
-  command 'https://s3.ap-south-1.amazonaws.com/girnarsoft-installs/secret_file /etc/chef/secret_key'
+  command 'aws s3 cp --region ap-south-1 s3://girnarsoft-installs/secret_file /etc/chef/secret_key'
   action :run
   not_if { File.exist?("/etc/chef/secret_key") }
   notifies :run, 'bash[add entry to client.rb]'
