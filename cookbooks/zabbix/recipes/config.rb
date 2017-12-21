ae6a5d7a5b381b6337a1b01006dd505c005a3e91
shruti <shruti.lamba@tothenew.com>
2017-12-21 09:11:42 +0530

    zabbix changes

--- cookbooks/zabbix/recipes/config.rb
+++ cookbooks/zabbix/recipes/config.rb
@@ -9,6 +9,7 @@ if node['platform'] == 'centos'
   execute 'selinux permissive' do
     command 'setenforce 0'
     action :run
+    only_if "getenforce | grep Enforcing"
   end
 end
 service 'zabbix-agent' do
