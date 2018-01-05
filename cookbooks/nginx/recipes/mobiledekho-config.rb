544acbe12eb75b032b9ddafbec053c9950b5092e
shruti <shruti.lamba@tothenew.com>
2018-01-05 12:23:47 +0530

    mobiledekho role

---
+++ cookbooks/nginx/recipes/mobiledekho-config.rb
@@ -0,0 +1,29 @@
+node['nginx'].each do |h|
+    v = h['vhost']
+    if v.has_key?('mobile_rule_root')
+      mobile_rule = true
+    else
+      mobile_rule = false
+    end
+      template "/etc/nginx/conf.d/#{v['main_domain']}.conf" do
+        source '/mobiledekho/vhost_config.erb'
+        owner 'root'
+        group 'root'
+        mode 00744
+        variables(
+          :server_aliases => v['aliases'],
+          :docroot => v['docroot'],
+          :access_log => v['access_log'],
+          :error_log => v['error_log'],
+          :rewrite_rule => v['rewrite_rule'],
+          :location_50x => v['location_50x'],
+          :jpg_rule => v['jpg_rule'],
+          :mobile_rule => mobile_rule,
+          :mobile_rule_root => v['mobile_rule_root']
+        )
+      end
+end
+
+service 'nginx' do
+  action :restart
+end
