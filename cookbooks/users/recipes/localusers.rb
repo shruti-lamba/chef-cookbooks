9dcbb10de252dba24baff0e1655908ffa291e3a3
shruti <shruti.lamba@tothenew.com>
2018-01-09 15:39:07 +0530

    localusers

---
+++ cookbooks/users/recipes/localusers.rb
@@ -0,0 +1,37 @@
+group 'superuser' do
+  gid 1004
+end
+
+node['users'].each do |user|
+    u = user['user_attributes']
+    username = u['username']
+    pub_key = u['pub_key']
+
+    home = "/home/#{username}"
+
+    user "#{username}" do
+          shell '/bin/bash'
+          home  home
+          manage_home true
+          gid 1004
+    end
+
+    directory "#{home}/.ssh" do
+          mode '0700'
+          owner "#{username}"
+          recursive true
+    end
+
+    file "#{home}/.ssh/authorized_keys" do
+          mode '0600'
+          owner "#{username}"
+          content "#{pub_key}"
+    end
+
+end
+
+execute 'create sudoer.d file' do
+  command 'echo "%superuser ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/superuser'
+  action :run
+  not_if { File.exist?("/etc/sudoers.d/superuser") }
+end
