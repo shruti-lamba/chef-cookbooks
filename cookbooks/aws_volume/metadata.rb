c14b3f3b2d47a4a1f50a839b045583e45968d8e0
shruti <shruti.lamba@tothenew.com>
2018-01-12 12:00:09 +0530

    ebs volume changes

--- cookbooks/aws_volume/metadata.rb
+++ cookbooks/aws_volume/metadata.rb
@@ -7,4 +7,4 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.1.5"
 #recipe 			  "aws", "Installs the aws-sdk gem during compile time"
 
-gem 'aws-sdk', '~> 3'
+gem 'aws-sdk', '2.10.47'
