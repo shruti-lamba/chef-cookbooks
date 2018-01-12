2c13e44f4e30915bab941b162e4c14b2c6d664da
shruti <shruti.lamba@tothenew.com>
2018-01-12 12:20:09 +0530

    changes

--- cookbooks/attach_ebs/recipes/default.rb
+++ cookbooks/attach_ebs/recipes/default.rb
@@ -3,20 +3,20 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-include_recipe 'aws_volume'
+#include_recipe 'aws_volume'
 
-include Aws::Ec2
+#include Aws::Ec2
 
-#aws_ebs_volume 'data_ebs_volume' do
-#  size 30
-#  device '/dev/sdf'
-#  volume_type "gp2"
-#  action [:create, :attach]
-#enda
-
-aws_volume_ebs_volume "data_ebs_volume" do
-	size 30
-	device "/dev/sda"
-	volume_type "gp2"
-	action [ :create, :attach ]
+aws_ebs_volume 'data_ebs_volume' do
+  size 30
+  device '/dev/sdf'
+  volume_type "gp2"
+  action [:create, :attach]
 end
+
+#aws_volume_ebs_volume "data_ebs_volume" do
+#	size 30#
+#device "/dev/sda"
+#	volume_type "gp2"
+#	action [ :create, :attach ]
+#end
