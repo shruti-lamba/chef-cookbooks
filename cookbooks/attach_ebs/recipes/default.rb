a94d8bc1aaec913c19bf09d90f3e27d867ab7828
shruti <shruti.lamba@tothenew.com>
2018-01-12 11:02:48 +0530

    metadata changes ebs cookbook

--- cookbooks/attach_ebs/recipes/default.rb
+++ cookbooks/attach_ebs/recipes/default.rb
@@ -3,9 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-chef_gem 'aws-sdk'
-
-aws_ebs_volume 'db_ebs_volume' do
+aws_ebs_volume 'data_ebs_volume' do
   size 30
   device '/dev/sdf'
   action [:create, :attach]
