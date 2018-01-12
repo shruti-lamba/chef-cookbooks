f232356065de28033e6a4fa184ff48cbf4045271
shruti <shruti.lamba@tothenew.com>
2018-01-12 10:47:53 +0530

    metadata changes ebs cookbook

--- cookbooks/attach_ebs/metadata.rb
+++ cookbooks/attach_ebs/metadata.rb
@@ -19,5 +19,4 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 #
 # source_url 'https://github.com/<insert_org_here>/attach_ebs'
 
-gem 'aws-sdk', '~> 3'
-depends 'aws'
+depends 'aws', '~> 3.0.1'
